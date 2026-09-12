1feafa9fa97309ccb551bd57e96be4fe84584af5
inaciosant <inaciosantana231@gmail.com>
2026-09-12 16:21:20 -0300

    faet: adicionando rate limiting e cors

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGO_URI'),
+      }),
+    }),
+    ProjectsModule,
+    AuthModule,
+    ],  
+  controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { rateLimit } from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 60 * 1000,
+      limit: 10,
+      standardHeaders: 'draft-8',
+      legacyHeaders: false,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
